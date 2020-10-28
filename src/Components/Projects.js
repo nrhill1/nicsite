9e10d23acecc472c6252fed0c3e92452d3ae835e
Nicolas Hill <nrhill1@gmail.com>
2020-10-27 17:09:13 -0700

    added more style changes and href to fw image

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,7 +7,11 @@ class Projects extends Component {
         return(
             <div className="projects">
                 <h2>Projects</h2>
-                <img className="fw" alt="Firewild" src={firewild}></img>
+                <a href="https://firewild.herokuapp.com/"><img className="fw" alt="Firewild" src={firewild}></img></a>
+                <p className="fwtext">Firewild is a wildfire tracking app that has full CRUD functionality when ran locally,
+                allowing users to post updates on a message board. I used React.js along with the Google Maps API and data 
+                from CA.gov to display polygons. The backend is made using Flask and MongoDB. The image is linked to the Heroku
+                version which does not support the backend capabilities as well as some of the style changes.</p>
             </div>
         )
     }
