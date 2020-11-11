ba4f75fc1131eb4678e9e0398ab1051d0c5bfac7
Nicolas Hill <nrhill1@gmail.com>
2020-11-11 13:11:46 -0800

    making some changes to firewild link

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,11 +7,10 @@ class Projects extends Component {
         return(
             <div className="projects">
                 <h2>Projects</h2>
-                <a href="https://firewild.herokuapp.com/"><img className="fw" alt="Firewild" src={firewild}></img></a>
+                <a href="https://gallant-wright-639beb.netlify.app/"><img className="fw" alt="Firewild" src={firewild}></img></a>
                 <p className="fwtext">Firewild is a wildfire tracking app that has full CRUD functionality when ran locally,
                 allowing users to post updates on a message board. I used React.js along with the Google Maps API and data 
-                from CA.gov to display polygons. The backend is made using Flask and MongoDB. The image is linked to the Heroku
-                version which does not support the backend capabilities as well as some of the style changes.</p>
+                from CA.gov to display polygons.</p>
             </div>
         )
     }
