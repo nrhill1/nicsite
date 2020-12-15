4f0c21436f78dcbec4cf8ce19211195abee323c5
Nicolas Hill <nrhill1@gmail.com>
2020-12-15 15:09:41 -0800

    changed firewild link

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,7 +7,7 @@ class Projects extends Component {
         return(
             <div className="projects">
                 <h2>Projects</h2>
-                <a href="firewild.netlify.app"><img className="fw" alt="Firewild" src={firewild}></img></a>
+                <a href={"https://firewild.netlify.app/"}><img className="fw" alt="Firewild" src={firewild}></img></a>
                 <p className="fwtext">Firewild is a wildfire tracking app that has full CRUD functionality when ran locally,
                 allowing users to post updates on a message board. I used React.js along with the Google Maps API and data 
                 from CA.gov to display polygons. I am currently working on a backend built with Flask and MongoDB, and I plan on 
